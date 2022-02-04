56785d0ba0c7322e41460ffeb80471b78c455f89
Daphne <daphnelink@gmail.com>
2022-02-04 14:18:12 -0800

    two projects and readme additions
    Random number generator app created and rolling dice app created. Also updated readme with new apps and images.

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Let's gnerate a random number between 1 and a number of your choice. Enter a number:");
+            int startingNumber = Convert.ToInt32(Console.ReadLine());
+
+            int randomNumber = random.Next(1, startingNumber);
+            Console.WriteLine($"Random Number: {randomNumber}");
+        }
+    }
+}
---
+++ RollDice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RollDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool play = true;
+            
+            while(play == true)
+            {
+                Console.WriteLine("Would you like to roll the dice? Enter Y for yes or N for no.");
+                string rollAgain = Console.ReadLine();
+
+                switch (rollAgain.ToUpper())
+                {
+                    case "Y":
+                        
+                        Random random = new Random();
+                        int diceRoll = random.Next(1, 6);
+                        Console.WriteLine($"You rolled: {diceRoll}");
+                        break;
+                    case "N":
+                        Console.WriteLine("Thank you for playing. See you next time.");
+                        play = false;
+                        break;
+                    default: Console.WriteLine("Invalid entry.");
+                        break;
+                }
+            }           
+        }
+    }
+}
