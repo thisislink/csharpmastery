29ea0d7a7e6436449f094bb4244d7c6efb75c06a
Daphne <daphnelink@gmail.com>
2022-02-04 16:02:38 -0800

    2 projects and readme added
    Added list of movies project to showcase use of arrays and a robot project to showcase the use of classes.

---
+++ FriendlyRobotHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FriendlyRobotHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot.IntroduceYourself();
+            string getName = Console.ReadLine();
+            Robot.SayGoodMorning(getName);
+            Robot.CleanKitchen();
+            Robot.MakeDinner();
+        }
+    }
+}
---
+++ ListMovies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ListMovies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] movies = { "Toy Story 3", "Terminator 2", "The Matrix" };
+            Console.WriteLine("Movies:");
+            for(int movie = 0; movie < movies.Length; movie++)
+            {
+                Console.WriteLine(movies[movie]);
+            }
+        }
+    }
+}
