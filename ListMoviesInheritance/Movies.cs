9c066801fd97784e486a86e3ebda20519af46365
Daphne Link <daphnelink@gmail.com>
2022-02-06 03:35:06 -0800

    updated list movies
    list movies project now includes inheritance example

---
+++ ListMoviesInheritance/Movies.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMoviesInheritance
+{
+    abstract internal class Movies
+    {
+        String title;
+        int year;
+        List<String> castMembers = new List<String>();
+
+        public Movies(String title, int year)
+        {
+            this.title = title;
+            this.year = year;
+        }
+
+        public void AddCastMember(string castMemberName)
+        {
+            castMembers.Add(castMemberName);
+            Console.WriteLine("New cast member added.");
+        }
+
+        public void DisplayCastMembers()
+        {
+            foreach (String castMember in castMembers)
+            {
+                Console.WriteLine(castMember);
+            }
+        }
+    }
+
+    class ComedyMovies : Movies
+    {
+        public ComedyMovies(String title, int year) : base(title, year)
+        {
+        }
+
+        public String genre = "Comedy";
+    }
+
+    class DramaMovies : Movies
+    {
+        public DramaMovies(String title, int year) : base(title, year)
+        {
+        }
+
+        public String genre = "Drama";
+    }
+
+    class HorrorMovies : Movies
+    {
+        public HorrorMovies(String title, int year) : base(title, year)
+        {
+        }
+
+        public String genre = "Horror";
+    }
+
+    class FamilyMovies : Movies
+    {
+        public FamilyMovies(String title, int year) : base(title, year)
+        {
+        }
+
+        public String genre = "Family";
+    }
+}
