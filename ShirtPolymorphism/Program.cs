b38f638b15e18d9d0733a5b59d0c2222c9cf897f
Daphne <daphnelink@gmail.com>
2022-02-07 14:11:44 -0800

    shirt polymorphism project
    small console app to display the concept of polymorphism

---
+++ ShirtPolymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShirtPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LongSleeveTShirt longSleeveTShirt = new LongSleeveTShirt();
+            ShortSleeveTShirt shortSleeveTShirt = new ShortSleeveTShirt();
+            ShortSleevePolo shortSleevePolo = new ShortSleevePolo();
+
+            Clothing[] clothes = { longSleeveTShirt, shortSleeveTShirt, shortSleevePolo };
+
+            foreach(Clothing item in clothes)
+            {
+                item.Color();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
