faee50641ed242187204b533f264aeccc4213bd4
Daphne <daphnelink@gmail.com>
2022-02-08 15:01:44 -0800

    new readme, updated old readme and new project
    added new project related to stacks and created new readme specific to data structures.

--- DataStructures/Stacks/Program.cs
+++ DataStructures/Stacks/Program.cs
@@ -1,58 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Stacks // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        Stack<string> _books = new Stack<string>();
-
         static void Main(string[] args)
         {
+            Stack<string> _books = new Stack<string>();
             Console.WriteLine("Type the name of a book and press ENTER:");
-            String bookName = Console.ReadLine();
-            AddToStack(bookName);
+            String bookName = Convert.ToString(Console.ReadLine());
+            AddToStack(bookName, _books);
 
             bool addMoreBooks = true;
             String yesNo = "";
             
             while (addMoreBooks)
             {
-                Console.WriteLine("\nWant to add more books?:");
-                Console.ReadLine();
+                Console.WriteLine("\nMenu:\n1. Type A and press ENTER to add another book \n2. Type R and press ENTER to delete the last book added " +
+                    "\n3. Type L and press ENTER to see the last book added \n4. Type D and press ENTER to display the entire list of books \n5. Type Q and press ENTER  to quit:");
+                yesNo = Console.ReadLine();
 
                 switch(yesNo.ToUpper())
                 {
-                    case "Y":
-                        
+                    case "A":
+                        Console.WriteLine("Type the name of a book and press ENTER:");
+                        bookName = Console.ReadLine();
+                        AddToStack(bookName, _books);
                         break;
-                        case "N":
+                    case "D":
+                        Console.WriteLine("Your current list of books:");
+                        DisplayAllBooks(_books);
                         break;
-                        default: 
-                            Console.WriteLine("\nInvalid entry!");
-                            break;
+                    case "R":
+                        Console.WriteLine(PopFromStack(_books));
+                        break;
+                    case "L":
+                        Console.WriteLine(PeekAtStack(_books));
+                        break;
+                    case "Q":
+                        addMoreBooks = false;
+                        Console.WriteLine("Here's your final list of books:");
+                        DisplayAllBooks(_books);
+                        break;
+                    default: 
+                        Console.WriteLine("\nInvalid entry!");
+                        break;
                 }
             }
             Console.ReadKey();
         }
 
-        public static void AddToStack(String book)
+        public static void AddToStack(String book, Stack<string> _books)
         {
             _books.Push(book);
+            Console.WriteLine($"\"{book}\" added to the list.");
         }
 
-        public static string PopFromStack(String book)
+        public static string PopFromStack(Stack<string> _books)
         {
             return _books.Pop();
         }
 
-        public static string PeekAtStack(String book)
+        public static string PeekAtStack(Stack<string> _books)
         {
             return _books.Peek();
         }
 
-        public string[] DisplayAllBooks()
+        public static void DisplayAllBooks(Stack<string> _books)
         {
-            return _books.ToArray();
+            foreach(String book in _books)
+            {
+                Console.WriteLine(book);
+            }            
         }
     }
 }
